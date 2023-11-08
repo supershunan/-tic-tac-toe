c6ca2990ee2d6dcc23f2dbc92539363436522166
王坤 <1552736550@qq.com>
2023-11-08 11:09:22 +0800

    chore: 测试amend

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-// 第三次sanci
+// 第三次sanci   测试 git commit --amend -m
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>,);
